35d67475bcbee027ab3f61db29a116bc811bffa7
bharath <33729709+bharath5673@users.noreply.github.com>
2018-07-18 22:11:56 +0530

    Update download.py
    (,encoding="utf8") updated which was unable to read in the current version IMDB
    
    #helpful

--- download.py
+++ download.py
@@ -117,10 +117,10 @@ def maybe_download_and_extract(url, download_dir):
 
         if file_path.endswith(".zip"):
             # Unpack the zip-file.
-            zipfile.ZipFile(file=file_path, mode="r").extractall(download_dir)
+            zipfile.ZipFile(file=file_path, mode="r",encoding="utf8").extractall(download_dir)
         elif file_path.endswith((".tar.gz", ".tgz")):
             # Unpack the tar-ball.
-            tarfile.open(name=file_path, mode="r:gz").extractall(download_dir)
+            tarfile.open(name=file_path, mode="r:gz",encoding="utf8").extractall(download_dir)
 
         print("Done.")
     else:
